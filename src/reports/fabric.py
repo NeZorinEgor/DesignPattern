d817f0d211279b6ba9d0feb4fc7d2445d3669ffd
NeZorinEgor <zorin-ep@mail.ru>
2024-09-27 10:22:42 +0800

    feat: add  method at ReportFactory

--- src/reports/fabric.py
+++ src/reports/fabric.py
@@ -30,7 +30,10 @@ class ReportFactory:
         self.error_proxy = ErrorProxy()
 
     @staticmethod
-    def set_format(report_format):
+    def create(report_format: FormatEnum):
+        """
+        Возвращает класс, в завивисомти от входного аргумента
+        """
         Validator.validate(report_format, FormatEnum)
 
         report_class = ReportFactory.report_classes.get(report_format)
@@ -39,26 +42,28 @@ class ReportFactory:
 
         return report_class()
 
-    def create(self, data):
-        """Создает отчет в зависимости от текущих настроек."""
-        try:
-            report_format = self.settings.report_format
-            report_class = self.set_format(report_format)
-            return report_class.create(data)
-        except Exception as e:
-            self.error_proxy.error_message = str(e)
-            return None
+    def create_default(self):
+        """
+        Возвращает класс в зависимости от значения по умолчанию из настроек
+        """
+        base_format = self.settings.report_format
+        report_class = ReportFactory.report_classes.get(base_format)
+        if report_class is None:
+            raise InvalidTypeError("Неподдерживаемый формат отчета")
+        return report_class()
+
 
 # TODO: add fixture to test
-# # Настройки для фабрики
-# settings_manager = SettingsManager()
-# settings_manager.from_json(os.path.join(os.pardir, "settings.json"))
-# # Создание инстанса рецепта
-# repository = DataRepository()
-# service = StartService(repository)
-# service.create()
-# recipe = repository.data[DataRepository.recipe_id()][0]
-# # Фабрика отчетности
-# factory = ReportFactory(settings_manager.settings)
-# creator = factory.set_format(FormatEnum.JSON)
-# print(creator.create(recipe))
+# Настройки для фабрики
+settings_manager = SettingsManager()
+settings_manager.from_json(os.path.join(os.pardir, "settings.json"))
+# Создание инстанса рецепта
+repository = DataRepository()
+service = StartService(repository)
+service.create()
+recipe = repository.data[DataRepository.recipe_id()][0]
+# Фабрика отчетности
+factory = ReportFactory(settings_manager.settings)
+creator = factory.create_default()
+# creator = factory.create(FormatEnum.JSON)
+print(creator.create(recipe))
--- src/reports/json_report.py
+++ src/reports/json_report.py
@@ -10,4 +10,4 @@ class JSONReport(ABCReport):
         fields = list(filter(lambda x: not x.startswith("_") and not callable(getattr(data.__class__, x)), dir(data)))
         result = {field: self._to_serializable(getattr(data, field)) for field in fields}
         # Возвращаем результат в формате JSON
-        return json.dumps(result, indent=4, ensure_ascii=False)
+        return json.dumps(result, indent=3, ensure_ascii=False)
